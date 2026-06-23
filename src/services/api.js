3bb606946161fa17b04769a1ad5012618deee3e7
adnexus-xo <aitadiagne333@gmail.com>
2026-06-23 12:16:35 +0000

    Fix: correct baseURL for axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://parc-applicatif-back-production.up.railway.app/api',
+  // Regarde bien le "-dfa2" ajouté ici pour correspondre à ton vrai lien Railway
+  baseURL: 'https://parc-applicatif-back-production-dfa2.up.railway.app/api',
 });
 
 export default api;
-
-
-
